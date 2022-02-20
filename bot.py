0556ebae10807f784a9cad0e014df98811ad67d8
Vadim <reliztoworld@gmail.com>
2022-02-20 11:42:43 +0300

    redid everything to continue doing this

--- bot.py
+++ bot.py
@@ -1,38 +1,40 @@
 import telebot
 from telebot import types
 
-bot = telebot.TeleBot("5112105842:AAGs0xlJIpYc0x_RcF3OblpnHn5MuwGm__A")
 
 name = ''
 surname = ''
 age = 0
 
+bot = telebot.TeleBot("5112105842:AAGs0xlJIpYc0x_RcF3OblpnHn5MuwGm__A")
 
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
-    bot.reply_to(message, "Вот мой список команд: /help,/start,Привет,/reg")
+    bot.reply_to(message, "Привет 😊, вот мой список команд: /reg, /help,/start")
 
 @bot.message_handler(func=lambda m: True)
 def echo_all(message):
     if message.text == 'Привет':
-        bot.reply_to(message, 'Привет, этот бот вам поможет ознакомлением репозитория- https://github.com/1o1dude/ABOBAbeta')
-    elif message.text == 'Hello':
-        bot.reply_to(message, 'Hi, this is bot helping you with learning a repozitoriy- https://github.com/1o1dude/ABOBAbeta')
+        bot.reply_to(message, 'привет привет 🤖')
+    elif message.text == 'hi':
+        bot.reply_to(message, 'Hi again! The bot creator!')
     elif message.text == '/reg':
-        bot.send_message(message.from_user.id, 'Привет, как тебя зовут?')
+        bot.send_message(message.from_user.id, "Привет,Как тебя зовут? 😨")
         bot.register_next_step_handler(message, reg_name)
+
+    #bot.reply_to(message, message.text)
 
 def reg_name(message):
     global name
     name = message.text
-    bot.send_message (message.from_user.id, 'понятненько, хм.. интересно, напишика свой игровой nick-name')
-    bot.register_next_step_handler (message, reg_surname)
+    bot.send_message(message.from_user.id, "назови-ка свой ник-нейм 🕹")
+    bot.register_next_step_handler(message, reg_surname)
 
 def reg_surname(message):
     global surname
     surname = message.text
-    bot.send_message (message.from_user.id, 'так, а теперь назови-ка свой возраст')
-    bot.register_next_step_handler (message, reg_age)
+    bot.send_message(message.from_user.id, "а теперь назови свой возраст 🤔")
+    bot.register_next_step_handler(message, reg_age)
 
 def reg_age(message):
     global age
@@ -41,13 +43,23 @@ def reg_age(message):
         try:
             age = int(message.text)
         except Exception:
-            bot.send_message (message.from_user.id, 'Пожалуйста, вводите текст цифрами')
-    bot.send_message (message.from_user.id, 'Тебе ' +str(age)+' лет? и тебя зовут ' +str(name)+' и твой ник? '+str(surname))
-    keyboard = types.InlineKeyboardMarup()
-    key_yes = types.InlineKeyboardMarup(text='Да', callback_data='Yes')
+            bot.send_message(message.from_user.id, "Вводите цифрами!")
+
+    keyboard = types.InlineKeyboardMarkup()
+    key_yes = types.InlineKeyboardButton(text='Да', callback_data='yes')
     keyboard.add(key_yes)
-    key_no = types.InlineKeyboardMarup (text='Нет', callback_data='No')
-    keyboard.add (key_no)
+    key_no = types.InlineKeyboardButton(text='Нет', callback_data='no')
+    keyboard.add(key_no)
+    question = 'Тебе ' + str(age) + ' лет? И тебя зовут: ' + name + ' ' + surname + '?'
+    bot.send_message(message.from_user.id, text = question, reply_markup=keyboard)
 
+@bot.callback_query_handler(func=lambda call: True)
+def callback_worker(call):
+    if call.data == "yes":
+        bot.send_message(call.message.chat.id, "Приятно познакомиться! Теперь запишу в БД!")
+    elif call.data == "no":
+        bot.send_message(call.message.chat.id, "Попробуем еще раз!")
+        bot.send_message(call.message.chat.id, "Привет! Давай познакомимся! Как тебя зовут?")
+        bot.register_next_step_handler(call.message, reg_name)
 
-bot.infinity_polling()
+bot.polling()
